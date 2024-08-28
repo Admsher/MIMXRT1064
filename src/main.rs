9d511f70909fedc8fb8d274f2bafafa7dcc8defe
Varad Shinde <99754991+Admsher@users.noreply.github.com>
2024-08-28 16:41:18 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,26 @@
-
-//! Prints "Hello, world!" on the host console using semihosting
-#![no_std]
 #![no_main]
+#![no_std]
 
+use imxrt_ral as ral;
+use imxrt_boot_gen as _;
 use imxrt_rt::entry;
-use imxrt_ral as pac;
 use panic_halt as _;
-use rtt_target::rprintln;
-use cortex_m as _;
-use imxrt1060evk_fcb as _;
-
+use imxrt_boot_gen as _;
 const LED_OFFSET: u32 = 1 << 11;
-
-// Register addresses come from the reference manual.
-const IOMUXC_MUX_CTL_PAD_GPIO_11: *mut u32 = 0x401F_8090 as _;
-const GPIO1_GDIR: *mut u32 = (0x401B_8000 + 0x04) as _;
-const GPIO1_DR_SET: *mut u32 = (0x401B_8000 + 0x84) as _;
 
 #[entry]
 fn main() -> ! {
-    rtt_target::rtt_init_print!();
-     unsafe {
-        // Configure the pad named "GPIO_11" as a GPIO pin
-        // (as opposed to a UART TX pin, for example).
-        IOMUXC_MUX_CTL_PAD_GPIO_11.write_volatile(5);
+    // Safety: we're the only code that "owns" the IOMUXC and GPIO1 peripherals.
+    let iomuxc = unsafe { ral::iomuxc::IOMUXC::instance() };
+    let gpio1 = unsafe { ral::gpio::GPIO1::instance() };
 
-        // Set the GPIO as an output with a RMW operation.
-        let mut gpio1_gdir = GPIO1_GDIR.read_volatile();
-        gpio1_gdir |= LED_OFFSET;
-        GPIO1_GDIR.write_volatile(gpio1_gdir);
+    // Configure the pad named "GPIO_11" as a GPIO pin
+    // (as opposed to a UART TX pin, for example).
+    ral::write_reg!(ral::iomuxc, iomuxc, SW_MUX_CTL_PAD_GPIO_B0_11, 5);
+    // Set the GPIO as an output with a RMW operation.
+    ral::modify_reg!(ral::gpio, gpio1, GDIR, |gdir| gdir | LED_OFFSET);
+    // Turn on the LED.
+    ral::write_reg!(ral::gpio, gpio1, DR_SET, LED_OFFSET);
 
-        // Turn on the LED.
-        GPIO1_DR_SET.write_volatile(LED_OFFSET);
-    }
-    
-    // Initialize FlexSPI
-
-    loop {
-        rprintln!("Hello, world!");
-    }
+    loop {}
 }
